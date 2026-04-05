07680f657c7fbbc49a9ad4e427bf482f7c3944aa
flaar <flaar@SQUMP>
2026-04-04 18:16:37 -0700

    made new scene

---
+++ Assets/WaveFunctionCollapse.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class WaveFunctionCollapse : MonoBehaviour
+{
+	enum States{
+		I,
+		E,
+		X
+	}
+	[SerializeField] int numCols;
+	[SerializeField] int numRows;
+
+
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
