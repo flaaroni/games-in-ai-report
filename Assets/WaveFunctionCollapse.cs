44e8854fda2a37393ac7c7f0d13c41c91197b212
flaar <flaar@SQUMP>
2026-04-04 20:15:19 -0700

    remove a state at random on start

--- Assets/WaveFunctionCollapse.cs
+++ Assets/WaveFunctionCollapse.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WaveFunctionCollapse : MonoBehaviour
@@ -10,29 +11,43 @@ public class WaveFunctionCollapse : MonoBehaviour
 	}
 
 	[SerializeField]
-	int numCols = 3;
+	int numCols = 4;
 	
 	[SerializeField]
-	int numRows = 3;
+	int numRows = 4;
 
-	private Tuple<List<States>, List<States>>[,] segments;
-
-	public Tuple<List<States>, List<States>>[,] Segments => segments;
+	private Tuple<List<States>, List<States>>[,] segmentsX;
+	private Tuple<List<States>, List<States>>[,] segmentsY;
+	public Tuple<List<States>, List<States>>[,] Segments => segmentsX;
+	public Tuple<States, States> this[int x, int y]
+	{
+		get
+		{
+			return new(Segments[x, y].Item1[0], Segments[x, y].Item2[0]);
+		}
+	}
 
 	void Start()
-    {
-        segments = new Tuple<List<States>, List<States>>[numCols, numRows];
-		for(int i = 0; i < numCols; i++)
+	{
+		segmentsX = new Tuple<List<States>, List<States>>[numCols, numRows];
+		segmentsY = new Tuple<List<States>, List<States>>[numCols, numRows];
+		allocateArray(segmentsX);
+		allocateArray(segmentsY);
+		System.Random r = new System.Random();
+		int row = r.Next(1, numRows - 1);
+		int col = r.Next(1, numCols - 1);
+		// 0 = left, 1 = right
+		int item = r.Next(2);
+		// 0 = column, 1 = row
+		int axis = r.Next(2);
+		while (isValidFace(col, row, item, axis))
 		{
-			for(int j  = 0; j < numRows; j++)
-			{
-				segments[i, j] = new Tuple<List<States>, List<States>>(new List<States>(), new List<States>());
-				segments[i, j].Item1.Add(States.I);
-				segments[i, j].Item1.Add(States.E);
-				segments[i, j].Item2.Add(States.I);
-				segments[i, j].Item2.Add(States.E);
-			}
+			row = r.Next(1, numRows - 1);
+			col = r.Next(1, numCols - 1);
+			item = r.Next(2);
+			axis = r.Next(2);
 		}
+		removeState(col, row, item, axis);
 	}
 
     // Update is called once per frame
@@ -40,4 +55,56 @@ public class WaveFunctionCollapse : MonoBehaviour
     {
         
     }
+
+	bool isValidFace(int col, int row, int item, int axis)
+	{
+		bool isLowestFace = (axis == 1 && item == 0 && row == 0) || (axis == 0 && item == 0 && col == 0);
+		bool isHighestFace = (axis == 1 && item == 1 && row == numRows - 1) || (axis == 0 && item == 1 && col == numCols - 1);
+		bool xAndChosen = (axis == 0 && (item == 0 && segmentsX[col, row].Item1.Count == 1) || (item == 1 && segmentsX[col, row].Item2.Count == 2));
+		bool yAndChosen = (axis == 1 && (item == 0 && segmentsY[col, row].Item1.Count == 1) || (item == 1 && segmentsY[col, row].Item2.Count == 2));
+		return xAndChosen || yAndChosen || isHighestFace || isLowestFace;
+	}
+
+	void allocateArray(Tuple<List<States>, List<States>>[,] array)
+	{
+		array = new Tuple<List<States>, List<States>>[numCols, numRows];
+		for (int i = 0; i < numCols; i++)
+		{
+			for (int j = 0; j < numRows; j++)
+			{
+				array[i, j] = new Tuple<List<States>, List<States>>(new List<States>(), new List<States>());
+				array[i, j].Item1.Add(States.I);
+				array[i, j].Item1.Add(States.E);
+				array[i, j].Item2.Add(States.I);
+				array[i, j].Item2.Add(States.E);
+			}
+		}
+	}
+
+	void removeState(int col, int row, int item, int axis)
+	{
+		if (item == 0)
+		{
+			if (axis == 0)
+			{
+				segmentsX[col, row].Item1.Remove(States.E);
+			}
+			else
+			{
+				segmentsY[col, row].Item1.Remove(States.E);
+			}
+
+		}
+		else
+		{
+			if (axis == 0)
+			{
+				segmentsX[col, row].Item2.Remove(States.E);
+			}
+			else
+			{
+				segmentsY[col, row].Item2.Remove(States.E);
+			}
+		}
+	}
 }
