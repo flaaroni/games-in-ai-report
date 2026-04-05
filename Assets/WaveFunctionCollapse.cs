e5fddf11d44d1b562d9d7b7fe48bda0942cfb3f9
flaar <flaar@SQUMP>
2026-04-04 18:57:43 -0700

    made data structure

--- Assets/WaveFunctionCollapse.cs
+++ Assets/WaveFunctionCollapse.cs
@@ -1,20 +1,39 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveFunctionCollapse : MonoBehaviour
 {
-	enum States{
+	public enum States{
 		I,
-		E,
-		X
+		E
 	}
-	[SerializeField] int numCols;
-	[SerializeField] int numRows;
 
+	[SerializeField]
+	int numCols = 3;
+	
+	[SerializeField]
+	int numRows = 3;
+
+	private Tuple<List<States>, List<States>>[,] segments;
+
+	public Tuple<List<States>, List<States>>[,] Segments => segments;
 
 	void Start()
     {
-        
-    }
+        segments = new Tuple<List<States>, List<States>>[numCols, numRows];
+		for(int i = 0; i < numCols; i++)
+		{
+			for(int j  = 0; j < numRows; j++)
+			{
+				segments[i, j] = new Tuple<List<States>, List<States>>(new List<States>(), new List<States>());
+				segments[i, j].Item1.Add(States.I);
+				segments[i, j].Item1.Add(States.E);
+				segments[i, j].Item2.Add(States.I);
+				segments[i, j].Item2.Add(States.E);
+			}
+		}
+	}
 
     // Update is called once per frame
     void Update()
