fd9f125f8c758aecb72aa5a2b6b4f15c0fe0396e
Taro Omiya <japtar10101@duck.com>
2026-04-13 03:11:01 -0700

    Adding comments.

--- Assets/Scripts/IGridGenerator.cs
+++ Assets/Scripts/IGridGenerator.cs
@@ -1,9 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public interface IGridGenerator
 {
+	/// <summary>
+	/// Checks if the current settings of this grid generator are valid for generating a grid.
+	/// </summary>
+	/// <returns></returns>
 	public bool IsValid();
+	/// <summary>
+	/// Generates a grid with a list of edges and faces.
+	/// </summary>
+	/// <param name="parent"></param>
+	/// <param name="groupPrefab"></param>
+	/// <param name="modelPrefab"></param>
+	/// <returns></returns>
 	public (IEnumerable<IEdge> edges, IEnumerable<IFace> faces) Generate(Transform parent, GameObject groupPrefab, GameObject modelPrefab);
 }
