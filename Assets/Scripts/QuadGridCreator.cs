e217e6a030f88ee093562e5f4017a71214a73550
Taro Omiya <taroomiya@duck.com>
2026-04-13 19:50:50 -0700

    Removing unused script and events.

--- Assets/Scripts/QuadGridCreator.cs
+++ Assets/Scripts/QuadGridCreator.cs
@@ -1,26 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuadGridCreator : MonoBehaviour
 {
-	public class QuadGridEventArgs : EventArgs
-	{
-		public QuadGridEventArgs(QuadGridCreator source, IEnumerable<IEdge> edges, IEnumerable<IFace> faces)
-		{
-			Source = source;
-			Edges = edges;
-			Faces = faces;
-		}
-
-		public QuadGridCreator Source { get; }
-		public IEnumerable<IEdge> Edges { get; }
-		public IEnumerable<IFace> Faces { get; }
-	}
-
-	public event Action<QuadGridEventArgs> OnMeshGenerated;
-
 	[Header("Grid Size")]
 	[SerializeField]
 	[Range(1, 10)]
@@ -62,7 +45,6 @@ public class QuadGridCreator : MonoBehaviour
 		(IEnumerable<IEdge> edges, IEnumerable<IFace> faces) = grid.Generate(transform, groupPrefab, modelPrefab);
 		modelSynthesis = new ModelSynthesis(faces, constraints);
 		modelSynthesis.Generate();
-		OnMeshGenerated?.Invoke(new QuadGridEventArgs(this, edges, faces));
 	}
 
 	[ContextMenu("Perform Wave Function Collapse")]
