a7bf9e2f41b49fee14968b9c051794da9f53892c
Taro Omiya <taroomiya@duck.com>
2026-04-13 17:53:27 -0700

    Pushing work-in-progress of the wave function collapse.

--- Assets/Scripts/Algorithms/ModelSynthesis.cs
+++ Assets/Scripts/Algorithms/ModelSynthesis.cs
@@ -1,16 +1,23 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
 
 public class ModelSynthesis
 {
+	class MaterialPair
+	{
+		public Material left;
+		public Material right;
+	}
+
 	// Constraints-related fields
 	readonly IReadOnlyList<Material> allMaterials;
+	readonly List<MaterialPair> allValidPairs;
 	readonly Constraints constraints;
 
 	// Possibility spaces
-	readonly SortedSet<IEdge> unObservedEdges;
-	readonly Dictionary<IEdge, HashSet<Material>> edgeToPossibleMaterials;
+	readonly HashSet<IEdge> unObservedEdges;
+	readonly Dictionary<IEdge, HashSet<MaterialPair>> edgeToPossibleMaterials;
 	readonly Dictionary<IFace, HashSet<Material>> faceToPossibleMaterials;
 
 	public ModelSynthesis(IEnumerable<IEdge> edges, IEnumerable<IFace> faces, Constraints constraints)
@@ -19,13 +26,24 @@ public class ModelSynthesis
 		this.constraints = constraints;
 		allMaterials = constraints.GetMaterials();
 
+		// Compute all the valid pairs of materials for edges
+		allValidPairs = new();
+		foreach (Material left in allMaterials)
+		{
+			ISet<Material> rightMaterials = constraints.GetTouchingMaterials(left);
+			foreach (Material right in rightMaterials)
+			{
+				allValidPairs.Add(new MaterialPair() { left = left, right = right });
+			}
+		}
+
 		// Add all edges with all possible materials to the list
 		unObservedEdges = new();
 		edgeToPossibleMaterials = new();
 		foreach (IEdge edge in edges)
 		{
 			unObservedEdges.Add(edge);
-			edgeToPossibleMaterials.Add(edge, new HashSet<Material>(allMaterials));
+			edgeToPossibleMaterials.Add(edge, new HashSet<MaterialPair>(allValidPairs));
 		}
 
 		// Add all faces with all possible materials to the list
@@ -40,10 +58,10 @@ public class ModelSynthesis
 	{
 		// Reset the list of unobserved edges and possible materials for each edge and face
 		unObservedEdges.Clear();
-		foreach ((IEdge edge, HashSet<Material> materials) in edgeToPossibleMaterials)
+		foreach ((IEdge edge, HashSet<MaterialPair> materials) in edgeToPossibleMaterials)
 		{
 			unObservedEdges.Add(edge);
-			materials.UnionWith(allMaterials);
+			materials.UnionWith(allValidPairs);
 		}
 		foreach ((IFace face, HashSet<Material> materials) in faceToPossibleMaterials)
 		{
@@ -56,8 +74,63 @@ public class ModelSynthesis
 		bool isGenerating = true;
 		do
 		{
+			// Choose a random edge to collapse
+			IEdge edge = GetRandomEdge();
+
+			// FIXME: collapse the edge
+			MaterialPair observedPair = CollapseEdge(edge);
+
+			// Remove the edge
+			unObservedEdges.Remove(edge);
+
+			// FIXME: update the faces of the edge
+			// FIXME: recursively propogate the constraints to neighboring edges and faces
+
+			// FIXME: confirm all edges and face still has at least 1 possible material
+			// , otherwise reset and start over
 			isGenerating = false;
 		}
-		while(isGenerating);
+		while (isGenerating);
+	}
+
+	IEdge GetRandomEdge()
+	{
+		// First, find the least number of possible materials for any unobserved edge
+		int minPossibilities = allMaterials.Count;
+		foreach (IEdge edge in unObservedEdges)
+		{
+			int numPossibilities = edgeToPossibleMaterials[edge].Count;
+			if (numPossibilities < minPossibilities)
+			{
+				minPossibilities = numPossibilities;
+			}
+		}
+
+		// Return any edges that meets this criteria
+		var toReturn = new List<IEdge>(unObservedEdges.Count);
+		foreach (IEdge edge in unObservedEdges)
+		{
+			if (minPossibilities == edgeToPossibleMaterials[edge].Count)
+			{
+				toReturn.Add(edge);
+			}
+		}
+		return toReturn[Random.Range(0, toReturn.Count)];
+	}
+
+	MaterialPair CollapseEdge(IEdge edge)
+	{
+		// Get all the possibilities for this edge
+		HashSet<MaterialPair> possiblePairs = edgeToPossibleMaterials[edge];
+
+		// Choose a random pair from the possibilities
+		MaterialPair observedPair = possiblePairs.ElementAt(Random.Range(0, possiblePairs.Count));
+
+		// Update the possibility dictionary
+		possiblePairs.Clear();
+		possiblePairs.Add(observedPair);
+
+		// Return the observed pair
+		return observedPair;
 	}
 }
